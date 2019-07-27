cb2eb4266c993ed856adfbcf5ea44f47fdc6edb5
Chloe Zhou <chloezhouny@gmail.com>
2019-07-26 23:39:13 -0700

    Basic logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    friends,
+    count: 0,
+    highScore: 0,
+    matchedFriends: [],
+    message: "Click to play!"
+  };
+
+  shuffleFriend = () => {
+    var tempCards = this.state.friends;
+
+    for (let i = tempCards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [tempCards[i], tempCards[j]] = [tempCards[j], tempCards[i]];
+    }
+    this.setState({ friends: tempCards });
+  };
+
+
+  checkhighScore = () =>
+  {
+    console.log("In check function");
+    var tempCount = this.state.count;
+    var tempHighScore = this.state.highScore;
+
+    console.log(tempCount);
+    console.log(tempHighScore);
+
+    if (tempCount > tempHighScore)
+    {
+       console.log(tempCount);
+
+      this.setState({ highScore : this.state.highScore + 1});
+
+    }
+  }
+
+
+  matchFriend = (id) => {
+
+     console.log(this.state.count);
+
+
+    var matchedFriends = this.state.matchedFriends;
+
+    if (matchedFriends[0] === null)
+    {
+
+       this.checkhighScore();
+      matchedFriends = [...matchedFriends, id];
+     
+
+
+      this.setState({ matchedFriends : matchedFriends});
+      this.setState({ count : this.state.count + 1});
+   
+
+    }
+    else
+    {
+      if (matchedFriends.includes(id))
+      {
+        this.setState({ count : 0 });
+        this.setState({ matchedFriends: []});
+
+        this.setState({message: "Game over. Start again."})
+        setTimeout(function() {this.setState({message: "Click to Play"});}.bind(this),3000);
+      }
+      else
+      {
+        this.setState({ count : this.state.count + 1});
+        // this.checkhighScore();
+
+
+        if (this.state.count < 3)
+        {
+          matchedFriends = [...matchedFriends, id];
+          this.setState({ matchedFriends : matchedFriends});
+          
+        }     
+        else if (this.state.count === 3)
+        {          
+          this.setState({ matchedFriends: []});  
+
+          this.setState({count: 0});
+          this.setState({message: "You win!"});
+          setTimeout(function() {this.setState({message: "Click to Play"});}.bind(this),3000);    
+        }
+      }
+    }
+
+  };
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        <div> Count: {this.state.count} </div>
+        <div> Total Count: 3 </div>
+        <div> High Score: {this.state.highScore} </div>
+        <div> {this.state.message} </div>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            shuffleFriend={this.shuffleFriend}
+            matchFriend={this.matchFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => {props.shuffleFriend(); props.matchFriend(props.id)}} className="remove">
+        ✓
+      </span>
+    </div>
+  );
+}
+
+export default FriendCard;
